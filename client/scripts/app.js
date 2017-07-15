ae2a449b6eb4b99d5d89ebc5e780a9e318438722
Hack Reactor Student <student@hackreactor.com>
2017-07-15 15:22:32 -0700

    friend add works now

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,17 +2,23 @@ var Chatterbox = function() {
   this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
   this.username = $(document)[0].URL.split('=')[1];
   this.currentMsgs;
+  this.friends = [];
 };
 
 Chatterbox.prototype.init = function() {
   var context = this;
   $(document).ready(function() {
-    $('#submitButton').on('click', function() {
+    // $('send').on('click'), app.handleSubmit();
+    // $('main').on('click'), app.handleUsernameClick();
+    $('#send').on('click', function() {
       context.handleSubmit();
     });
     $('#roomSelect').on('click', function() {
       context.renderRoom();
     });
+    // $('#chats a').on('click', function() {
+    //   context.handleUsernameClick(this.value);
+    // });
   });
   this.fetch();
 };
@@ -57,8 +63,24 @@ Chatterbox.prototype.clearMessages = function() {
 
 Chatterbox.prototype.renderMessage = function(message) {
   var $child = document.createElement('div');
-  $child.innerHTML += '<b><br>' + message.username + ': </b><br>' + message.text + 'Room :' 
-    + '<br>' + message.roomname + 'Time Posted: ' + message.createdAt;
+
+  var username = message.username;
+  if (username.includes('-') || username.includes('<') || username.includes('>') || username.includes('&') || username.includes('"')) {
+    username = 'invalid name';
+  } else {
+    if (app.friends.includes(message.username)) {
+      username = '<b>' + username + '</b>';
+    }
+  }
+
+  var messageText = message.text;
+  if (messageText !== undefined) {
+    if (messageText.includes('-') || messageText.includes('<') || messageText.includes('>') || messageText.includes('&') || messageText.includes('"')) {
+      messageText = 'invalid message';
+    }
+  }
+  
+  $child.innerHTML += '<br><a onClick="app.handleUsernameClick(this.innerText)" href="#">' + username + ': </a>' + '<br>' + messageText + '<br>' + 'Room: ' + message.roomname + 'Time Posted: ' + message.createdAt;
   $('#chats').prepend($child);
 };
 
@@ -81,13 +103,16 @@ Chatterbox.prototype.renderRoom = function(roomName) {
   }
 };
 
-Chatterbox.prototype.handleUsernameClick = function(message) {
+Chatterbox.prototype.handleUsernameClick = function(friendName) {
+  if (!this.friends.includes(friendName)) {
+    this.friends.push(friendName);
+  }
 };
 
 Chatterbox.prototype.handleSubmit = function(message) {
   var msgObject = {
     username: this.username,
-    text: document.getElementById('textBox').value,
+    text: document.getElementById('message').value,
     roomname: $('#roomSelect').find(':selected').text()
   };
   this.renderMessage(msgObject);
